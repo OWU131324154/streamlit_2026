e06919fc0c507f4e7b76cb2914d1553e343f1cf9
OWU131324154 <s1324154@cst.otsuma.ac.jp>
2026-06-09 09:13:21 +0900

    Add files via upload

---
+++ test2-3.py
@@ -0,0 +1,107 @@
+name ="大妻 花子"
+age = 20
+is_student = False
+
+x, y, z = 1, 2, 3
+
+age = 21
+
+print(name)
+print(age)
+print(is_student)
+print(x, y, z)
+
+age = 20
+count = -5
+
+height = 162.5
+pi = 3.14159265359
+
+print(age)
+print(count)
+print(height)
+print(pi)
+
+print(age + count)
+
+name = "大妻 花子"
+message = 'pythonを学んでいます'
+multiline = """一行目
+２行目
+３行目
+"""
+
+print(name)
+print(message)
+print(multiline)
+
+is_student = True
+has_finished = False
+
+result = None
+
+print(is_student)
+print(has_finished)
+print(result)
+
+print(type(name))
+
+age_str = "20"
+print(age_str)
+print(type(age_str))
+
+#文字列を数値に変換
+age_int = int(age_str)
+print(age_int)
+print(type(age_int))
+
+x = 3
+#print(x + age_str)
+print(x + age_int)
+
+int("1234")
+#int("abc")は無理
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+x = 10
+# X = x + 5
+x += 5
+print(x)
+
+a = 10
+b = 5
+
+if a == b:
+    print("aとbは等しい")
+else:
+    print("aとbは等しくない")
+
+
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+print(a != b)
+
+if (a == 10 and b == 5):
+    prnt("True")
+else:
+    print("False")
+
+a = "10"
+b = "45"
+
+print(a + b)
+
+message = "abcde"
+print(message.upper())
+
+message = "abcde".replace("a", "A")
+print(message)
